a43a3c724cac540262e9bf9790e2239a5f032cfa
DeinMutter <108142589+DeinMutter@users.noreply.github.com>
2023-04-26 22:59:55 -0400

    Update VisualPanel.java

--- VisualPanel.java
+++ VisualPanel.java
@@ -1,21 +1,53 @@
+	/*****************************************************************
+	* The visual panel for the ships, allows the shooting from the player
+   * Also denotates where all of the guesses have been utilizing the markers
+	 
+	* @Rohan Singh, Fabeun Rahman and Arrush Shah
+	* @version 4/25/23
+	****************************************************************/
+
+
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
 
+
 public class VisualPanel {
-
-}
-
-public void shoot(int x, int y){
+   /**
+   * Where all of the information will go in, as far as the visuals
+   */
+   public VisualPanel()
+   {
+   }
+   
+   /**
+   * Applies a shot onto another board
+   */
+   public void shoot(int x, int y){
     
-}
-public void sink(int[][] points){
+   }
+   /**
+   * Makes a ship disappear
+   */
+   
+   public void sink(int[][] points){
 
-}
-public void place(int[][] points){
+   }
+   /**
+   * Sets the ships onto the board
+   */
+   
+   public void place(int[][] points){
 
-}
-public void recieveHit(int x, int y){
+   }
+   
+   /**
+   * Takes in a shot to apply to the board
+   */
+   public void recieveHit(int x, int y){
 
+   }
 }
+
