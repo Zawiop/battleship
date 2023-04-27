cc9e2aba041b68eb10d54db0f1c4633b88bd2737
Arrush Shah <56138833+Zawiop@users.noreply.github.com>
2023-04-26 21:40:42 -0500

    Fixed Shoot

---
+++ VisualPanel.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+
+public class VisualPanel {
+
+}
+
+public void shoot(int x, int y){
+    
+}
+public void sink(int[][] points){
+
+}
+public void place(int[][] points){
+
+}
+public void recieveHit(int x, int y){
+
+}
