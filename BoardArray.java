b026426fbe2fea35b106643b627c162a6bdd9b13
Arrush Shah <56138833+Zawiop@users.noreply.github.com>
2023-05-03 15:27:01 -0400

    Finished BoardArray

--- BoardArray.java
+++ BoardArray.java
@@ -14,13 +14,29 @@ public class BoardArray {
    * The 2-d array that represents the scoreboard itself
    */
 
-    private int[][] scoreboard;
+    private int[][] scoreboard = new int[7][7];
    /**
    * Takes a move that has happened, and is applied to the array itself
    */
+   public BoardArray() {
+      int i =0;
+      int j = 0;
+      while(i < 7) {
+         while(j < 7) {
+            scoreboard[i][j] = 0;
+            j++;
+         }
+         i++;
+      }
+   }
     
-    public void setMove(int guess, int value)
+    public void setMove(int xofG, int yofG, int guess)
     {
+      scoreboard[xofG][yofG] = guess;
+      // 0 == no ship, not hit
+      // 1 == no ship, hit
+      // 2 == ship, no hit
+      // 3 == ship, hit
     }
     
     
@@ -28,9 +44,14 @@ public class BoardArray {
    * Checks if the selected position has a ship or not, simplifies lots of operations (false is a placeholder)
    */
     
-    public boolean isShip(String position)
+    public boolean isShip(int x, int y)
     {
-      return false;
+      if(scoreboard[x][y] == 2 || scoreboard[x][y] == 3) {
+         return true;
+      }
+      else {
+       return false;
+      }
     }
     
    /**
@@ -38,15 +59,17 @@ public class BoardArray {
    */
     public int[][] getScoreboard()
     {
-      return null;
+      return scoreboard;
     }
     
     /**
     * Sets ships onto the board and adds them to the scoreboard private variable
     */
-    public void placeShape(String Positions, String shipType)
+    public void placeShape(int [][] values, int n)
     {
-    
+      for(int i = 0; i < n; i++) {
+         scoreboard[values[i][0]][values[i][1]] = 2;
+      }
     }
     
 
