0838e3f11824ce9dbb75b857db24cc650cbc355d
Arrush Shah <56138833+Zawiop@users.noreply.github.com>
2023-05-05 10:19:21 -0400

    Final Panel Progress + Board

--- FinalPanel.java
+++ FinalPanel.java
@@ -5,15 +5,73 @@
 	* @version 4/25/23
 	****************************************************************/
 
+import javax.swing.*;
+import java.awt.*;
+import java.util.concurrent.TimeUnit;
+import java.net.*;
+import java.awt.event.*;
 
-public class FinalPanel
-{
+
+public class FinalPanel extends JPanel {
+   static boolean gameStarted = false;
+   
    /**
    * Where all elements will be instantiated and added, located in the constructor for default behavior
    */
    public FinalPanel()
    {
-   }
-
    
+   
+   }
+   public void gameStart(Graphics g) {
+      if(gameStarted == false) {
+         JButton help = new JButton("Instructions Menu");
+         JButton set = new JButton("Set Ships");
+         JButton reset = new JButton("Reset Board");
+         help.addActionListener(new helpMenu());
+         
+         add(help);
+
+         ImageIcon icon = new ImageIcon("board.JPG");
+         JLabel label = new JLabel(icon);
+         label.setBounds(-20,-20, 640,640);
+         add(label);
+         
+
+
+         
+
+         //gameStarted = true;
+      }
+      }
+   public void playGame(Graphics g) {
+      if(gameStarted == true) {
+         g.setColor(new Color(103,179,201));
+         g.drawRect(50,50,90,70);
+      }
+      
+   
+   }
+   public void paintComponent(Graphics g) {
+      gameStart(g);
+      playGame(g);
+    
 }
+   public static void main(String[] args)  throws MalformedURLException {
+   
+   }
+  
+
+ private  class helpMenu implements ActionListener {
+      /**
+      * What the help menu button will actually do 
+      */
+       public void actionPerformed(ActionEvent e) {
+         HelpPanelContainer def = new HelpPanelContainer();
+         JPanel panel = def.newJpanel();
+         //frame.add(panel); 
+         //whenever we add the frame in
+       }
+   }  
+}
+
