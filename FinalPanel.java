dee70e31f73a20d5611afa87761349c5cc410141
Arrush Shah <shaharrush@gmail.com>
2023-05-22 23:30:31 -0400

    dying

--- FinalPanel.java
+++ FinalPanel.java
@@ -19,10 +19,17 @@ static boolean gameStarted = false;
 static JFrame frame = new JFrame("hey");
 static int xClicked, yClicked, counterX, counterY, xCount;
 static int[][] boundsArray = new int[100][4];
-static JLabel boardLabel, c2label, s2label, d2label;
+static JLabel boardLabel;
+static JLabel d2label = new JLabel();
+static JLabel s2label = new JLabel();
+static JLabel c2label = new JLabel();
+static JLabel car2label = new JLabel();
+static JLabel b2label = new JLabel();
 static int carrierRotation = 1;
 static int subRotation = 1;
 static int destroyerRotation = 1;
+static boolean carrier, sub, battleship, destroyer, crusier;
+static String selected = "none";
 
 static int xTL1 = 70;
 static int yTL1 = 64;
@@ -191,7 +198,7 @@ public void subListener(MouseEvent e){
       int y = boundsArray[searched][1];
       s2label.setBounds(x-53-188-8 - scaled + bounds, y-190-98-8 + scaled - bounds, 640, 640);
       frame.add(s2label);
-      c2label.setVisible(true);
+      s2label.setVisible(true);
       Container container = s2label.getParent();
       container.setComponentZOrder(s2label, 0);
       container.repaint();
@@ -236,9 +243,8 @@ public void destroyerListener(MouseEvent e){
          bounds = Integer.parseInt((String)array[2]);
          destroyerRotation++;
 
-      } else {
-            d2Scaled = null;
-         
+      }else {
+         d2Scaled = null;
       }
       ImageIcon deIcon = new ImageIcon(d2Scaled);
       d2label.setVisible(false);
@@ -260,16 +266,35 @@ public void destroyerListener(MouseEvent e){
    
 
 }
+public void battleshipListener(MouseEvent e){
+}
+public void carrierListener(MouseEvent e){
+}
+
+
 
 
 public void gameStart() {
    if(gameStarted == false) { 
+      create();
       addMouseListener(
          new MouseAdapter() {
             public void mouseClicked(MouseEvent e) {
-            //crusierListener(e);
-            //subListener(e);  
-            destroyerListener(e);
+             
+            if(selected == "destroyer"){
+               destroyerListener(e);
+            } else if(selected == "carrier"){
+               carrierListener(e);
+            } else if(selected == "cruiser"){
+               System.out.println("hey");
+               crusierListener(e);
+            } else if(selected == "battleship"){
+               battleshipListener(e);
+            } else if(selected == "submarine"){
+               subListener(e);
+            } else {
+               
+            }
             }
                
                
@@ -281,94 +306,105 @@ public void gameStart() {
          
          );
       
+         }
+      }
+      public void create(){      
+         JButton help = new JButton("Instructions Menu");
+         JButton set = new JButton("Set Ships");
+         JButton reset = new JButton("Reset Board");
+         JButton deSelectShip = new JButton("Deselect Ship");
+         help.addActionListener(new helpMenu());
+         set.addActionListener(new setShips());
+         reset.addActionListener(new resetShips());
+         deSelectShip.addActionListener(new deSelect());
+         set.setBounds(700, 50, 150, 25);
+         help.setBounds(700, 0, 150, 25);
+         reset.setBounds(700, 100, 150, 25);
+         deSelectShip.setBounds(900, 50, 150, 25);
+         add(help);
+         add(set);
+         add(reset);
+         add(deSelectShip);
       
-      JButton help = new JButton("Instructions Menu");
-      JButton set = new JButton("Set Ships");
-      JButton reset = new JButton("Reset Board");
-      help.addActionListener(new helpMenu());
-      set.addActionListener(new setShips());
-      reset.addActionListener(new resetShips());
-      set.setBounds(700, 50, 150, 25);
-      help.setBounds(700, 0, 150, 25);
-      reset.setBounds(700, 100, 150, 25);
+         ImageIcon board = new ImageIcon("board.JPG");
+         boardLabel = new JLabel(board);
+         boardLabel.setBounds(-20, -20, 640,640);
+         //add(boardLabel);
       
-      add(help);
-      add(set);
-      add(reset);
-   
-      ImageIcon board = new ImageIcon("board.JPG");
-       boardLabel = new JLabel(board);
-      boardLabel.setBounds(-20, -20, 640,640);
-      //add(boardLabel);
-   
-      ImageIcon sub = new ImageIcon("SubmarineNoBG.PNG");
-      Image subScaling = sub.getImage();
-      Image newSub = subScaling.getScaledInstance(133, 46, java.awt.Image.SCALE_SMOOTH);
-      sub = new ImageIcon(newSub);
-      //180 degrees
-      //JLabel subLabel = new JLabel(rotateIcon(rotateIcon(sub,90), 90));
-      //270 degrees
-      s2label = new JLabel(sub);
-      s2label.setBounds(450, 20, 640,640);
+         ImageIcon sub = new ImageIcon("SubmarineNoBG.PNG");
+         Image subScaling = sub.getImage();
+         Image newSub = subScaling.getScaledInstance(133, 46, java.awt.Image.SCALE_SMOOTH);
+         sub = new ImageIcon(newSub);
+         //180 degrees
+         //JLabel subLabel = new JLabel(rotateIcon(rotateIcon(sub,90), 90));
+         //270 degrees
+         JLabel sub2 = new JLabel(sub);
+         sub2.addMouseListener(new subClicked());
+         sub2.setBounds(640, 320, 270,55);
+         
+         
+         ImageIcon bship = new ImageIcon("BattleshipNoBG.png");
+         Image bScale = bship.getImage();
+         Image bScaled = bScale.getScaledInstance(185, 31, java.awt.Image.SCALE_SMOOTH);
+         bship = new ImageIcon(bScaled);
+         JLabel b2 = new JLabel(bship);
+         b2.addMouseListener(new battleshipClicked());
+         b2.setBounds(640, 240, 270, 55);
+         
+         ImageIcon carrier = new ImageIcon("carrierNoBG.png");
+         Image cScale = carrier.getImage();
+         Image cScaled = cScale.getScaledInstance(240, 50, java.awt.Image.SCALE_SMOOTH);
+         carrier = new ImageIcon(cScaled);
+         JLabel clabel = new JLabel(carrier);
+         clabel.setBounds(640, 175, 270, 55);
+         
+         ImageIcon cruiser = new ImageIcon("cruiserNoBG.png");
+         Image c2Scale = cruiser.getImage();
+         Image c2Scaled = c2Scale.getScaledInstance(140, 45, java.awt.Image.SCALE_SMOOTH);
+         cruiser = new ImageIcon(c2Scaled);
+         JLabel c2 = new JLabel(cruiser);
+         c2.addMouseListener(new cruiserClicked());
+         
+         c2.setBounds(640, 400, 270, 55);
+         
+         ImageIcon destroyer = new ImageIcon("destroyerNoBG.png");
+         Image dScale = destroyer.getImage();
+         Image dScaled = dScale.getScaledInstance(90, 30, java.awt.Image.SCALE_SMOOTH);
+         destroyer = new ImageIcon(dScaled);
+         JLabel d2label = new JLabel(destroyer);
+         d2label.setBounds(640, 470, 270, 55);
+         //d2label.setBounds(17, -96, 17 + 190, -96+460); //This is (70, 64) starting point with 190 460 scale applied to it 
+         
+         ImageIcon frame = new ImageIcon("frameNoBG.png");
+         Image fScale = frame.getImage();
+         Image fScaled = fScale.getScaledInstance(500, 500, java.awt.Image.SCALE_SMOOTH);
+         frame = new ImageIcon(fScaled);
+         JLabel flabel = new JLabel(frame);
+         flabel.setBounds(455, 30, 640, 640);
+         
+         
+      
+         
+         
+         add(flabel);
+         add(sub2);
+         add(b2);
+         add(clabel);
+         add(c2);
+         add(d2label);
+         add(flabel);
+         //add(subLabel);
+         add(boardLabel);
+         
       
       
-      ImageIcon bship = new ImageIcon("BattleshipNoBG.png");
-      Image bScale = bship.getImage();
-      Image bScaled = bScale.getScaledInstance(185, 31, java.awt.Image.SCALE_SMOOTH);
-      bship = new ImageIcon(bScaled);
-      JLabel blabel = new JLabel(bship);
-      blabel.setBounds(450, -55, 640, 640);
+         // Next steps, create bship and carrier, implement them, and add their listeners accordingly
+         // Debug rotating +4
+         // and do set ships 
       
-      ImageIcon carrier = new ImageIcon("carrierNoBG.png");
-      Image cScale = carrier.getImage();
-      Image cScaled = cScale.getScaledInstance(240, 50, java.awt.Image.SCALE_SMOOTH);
-      carrier = new ImageIcon(cScaled);
-      JLabel clabel = new JLabel(carrier);
-      clabel.setBounds(450, -125, 640, 640);
-      
-      ImageIcon cruiser = new ImageIcon("cruiserNoBG.png");
-      Image c2Scale = cruiser.getImage();
-      Image c2Scaled = c2Scale.getScaledInstance(140, 45, java.awt.Image.SCALE_SMOOTH);
-      cruiser = new ImageIcon(c2Scaled);
-      c2label = new JLabel(cruiser);
-      c2label.setBounds(450, 110, 640, 640);
-      
-      ImageIcon destroyer = new ImageIcon("destroyerNoBG.png");
-      Image dScale = destroyer.getImage();
-      Image dScaled = dScale.getScaledInstance(90, 30, java.awt.Image.SCALE_SMOOTH);
-      destroyer = new ImageIcon(dScaled);
-      d2label = new JLabel(destroyer);
-      d2label.setBounds(450, 180, 640, 640);
-      //d2label.setBounds(17, -96, 17 + 190, -96+460); //This is (70, 64) starting point with 190 460 scale applied to it 
-      
-      ImageIcon frame = new ImageIcon("frameNoBG.png");
-      Image fScale = frame.getImage();
-      Image fScaled = fScale.getScaledInstance(500, 500, java.awt.Image.SCALE_SMOOTH);
-      frame = new ImageIcon(fScaled);
-      JLabel flabel = new JLabel(frame);
-      flabel.setBounds(455, 30, 640, 640);
-      
-      
-   
-      
-      
-      add(flabel);
-      add(s2label);
-      add(blabel);
-      add(clabel);
-      add(c2label);
-      add(d2label);
-      add(flabel);
-      //add(subLabel);
-      add(boardLabel);
-      
-   
-   
-      
-   
-      //gameStarted = true;
-   }
-}
+         //gameStarted = true;
+      }
+
 public void playGame(Graphics g) {
    if(gameStarted == true) {
       g.setColor(new Color(103,179,201));
@@ -419,28 +455,93 @@ private class resetShips implements ActionListener {
    /**
    * What the setShips button will actually do 
    */
-   public void actionPerformed(ActionEvent e) {
+  public void actionPerformed(ActionEvent e) {
      
+  }
+}
+private class deSelect implements ActionListener {
+   public void actionPerformed(ActionEvent e) {
+      selected = "none";
    }
 }
+private class subClicked implements MouseListener {
+   public void mouseClicked(MouseEvent e) {
+      selected = "submarine";
+     }
 
-private ImageIcon rotateIcon(ImageIcon icon, int angle) {
-   Image image = icon.getImage();
-   int width = image.getWidth(null);
-   int height = image.getHeight(null);
+  public void mousePressed(MouseEvent e) {}
 
-   BufferedImage rotatedImage = new BufferedImage(height, width, BufferedImage.TYPE_INT_ARGB);
-   Graphics2D g2d = rotatedImage.createGraphics();
+  public void mouseReleased(MouseEvent e) {}
 
-   g2d.translate(height / 2, width / 2);
-   g2d.rotate(Math.toRadians(angle));
-   g2d.translate(-width / 2, -height / 2);
+  public void mouseEntered(MouseEvent e) {}
 
-   g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-   g2d.drawImage(image, 0, 0, null);
-   g2d.dispose();
-
-   return new ImageIcon(rotatedImage);
-
+  public void mouseExited(MouseEvent e) {}
+     
+  
 }
+private class destroyerClicked implements MouseListener {
+   public void mouseClicked(MouseEvent e) {
+      selected = "destroyer";
+     }
+
+  public void mousePressed(MouseEvent e) {}
+
+  public void mouseReleased(MouseEvent e) {}
+
+  public void mouseEntered(MouseEvent e) {}
+
+  public void mouseExited(MouseEvent e) {}
+     
+  
+}
+private class battleshipClicked implements MouseListener {
+   public void mouseClicked(MouseEvent e) {
+      selected = "battleship";
+      System.out.println("tks");
+     }
+
+  public void mousePressed(MouseEvent e) {}
+
+  public void mouseReleased(MouseEvent e) {}
+
+  public void mouseEntered(MouseEvent e) {}
+
+  public void mouseExited(MouseEvent e) {}
+     
+  
+}
+private class cruiserClicked implements MouseListener {
+   public void mouseClicked(MouseEvent e) {
+      selected = "cruiser";
+      System.out.println("cruiser");
+     }
+
+  public void mousePressed(MouseEvent e) {}
+
+  public void mouseReleased(MouseEvent e) {}
+
+  public void mouseEntered(MouseEvent e) {}
+
+  public void mouseExited(MouseEvent e) {}
+     
+  
+}
+private class carrierClicked implements MouseListener {
+   public void mouseClicked(MouseEvent e) {
+      selected = "carrier";
+     }
+
+  public void mousePressed(MouseEvent e) {}
+
+  public void mouseReleased(MouseEvent e) {}
+
+  public void mouseEntered(MouseEvent e) {}
+
+  public void mouseExited(MouseEvent e) {}
+     
+  
+}
+
+
+
 }
