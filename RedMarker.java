5e653e8c1bebf1f94259f30fbe06ed823d5ba94d
DeinMutter <108142589+DeinMutter@users.noreply.github.com>
2023-05-24 00:39:57 -0400

    Add files via upload

--- RedMarker.java
+++ RedMarker.java
@@ -1,16 +1,24 @@
-	/*****************************************************************
+import javax.swing.*;
+import java.awt.*;
+import java.util.concurrent.TimeUnit;
+import java.net.*;
+import java.awt.event.*;
+import java.awt.Image.*;
+import java.awt.image.BufferedImage;
+
+/*****************************************************************
 	* An extention of an abstract class, simply extends the Battleship classes
    * Does not have any unique methods of its own
    * Is not a ship itself, but extends the ship class due to commonality of methods
 	 
-	* @Rohan Singh, Fabeun Rahman and Arrush Shah
+	* @Authors Rohan Singh, Fabeun Rahman and Arrush Shah
 	* @version 4/25/23
 	****************************************************************/
 
 
 
 
-public class RedMarker extends Battleships{
+public class RedMarker extends JPanel{
    /**
    * Marker's x coordinate
    */
@@ -22,22 +30,38 @@ public class RedMarker extends Battleships{
       private int myY;
       public RedMarker(int x, int y)
       {
-         myX = x;
-         myY = y;
+         ImageIcon bruh = new ImageIcon("fire.gif");
+         JLabel bruh2 = new JLabel(bruh);
+         add(bruh2);
       }
       
    /**
    * Shows the marker at the specified coordinates on the panel
    */
       
-      public void show(int x, int y)
+      public Object[] show(int x, boolean left)
       {
+         return null;
       }
       
    /**
    * Will actually stay as a blank definition, because this is not necessary for a circular marker
    */
-      public void rotate(int z)
+      public ImageIcon rotate(ImageIcon icon, int angle)
       {
+         return null;
+      }
+
+      public static void main(String[] args)
+      {
+         ImageIcon bruh = new ImageIcon("fire.gif");
+         // frame.setSize(1500, 600);
+         // frame.setLocation(0, 0);
+         // frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+         // frame.setBackground(Color.RED);
+
+   
+         // frame.setContentPane(new FinalPanel());
+         // frame.setVisible(true);
       }
 }   
--- WhiteMarker.java
+++ WhiteMarker.java
@@ -1,9 +1,16 @@
-	/*****************************************************************
+import javax.swing.*;
+import java.awt.*;
+import java.util.concurrent.TimeUnit;
+import java.net.*;
+import java.awt.event.*;
+import java.awt.Image.*;
+import java.awt.image.BufferedImage;	
+   /*****************************************************************
 	* An extention of an abstract class, simply extends the Battleship classes
    * Does not have any unique methods of its own
    * Is not a ship itself, but extends the ship class due to commonality of methods
 	 
-	* @Rohan Singh, Fabeun Rahman and Arrush Shah
+	* @Authors Rohan Singh, Fabeun Rahman and Arrush Shah
 	* @version 4/25/23
 	****************************************************************/
 
@@ -30,14 +37,21 @@ public class WhiteMarker extends Battleships{
    * Shows the marker at the specified coordinates on the panel
    */
       
-      public void show(int x, int y)
-      {
-      }
+   public Object[] show(int x, boolean left)
+   {
+      ImageIcon marker = new ImageIcon("whiteMarker.png");
+      JLabel jmarker = new JLabel(marker);
+      Object[] returnVal = new Object[1];
+      returnVal[0] = jmarker;
+      //-53-188-8+23 setbounds and figure it out!!
       
+      return returnVal;
+   }
    /**
    * Will actually stay as a blank definition, because this is not necessary for a circular marker
    */
-      public void rotate(int z)
-      {
-      }
+  public ImageIcon rotate(ImageIcon icon, int angle)
+  {
+     return null;
+  }
 }   
