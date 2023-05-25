093af37961617c2103295d1216b92b4c3c605e13
DeinMutter <108142589+DeinMutter@users.noreply.github.com>
2023-05-24 22:44:08 -0400

    Add files via upload

--- Robot.java
+++ Robot.java
@@ -1,4 +1,8 @@
 import java.util.Random;
+import java.io.*;
+import java.util.Arrays;
+import java.math.*;
+
 
 
 
@@ -18,180 +22,126 @@ public class Robot
    /**
    * Simply marking where the ships are, for the robot's reference
    */
-   private int[] guessed;
-   private int turnCounter = 0;
-   private int[] posOfGuesses;
+   private static int[] guessed = new int[100];
+   private static int turnCounter = 0;
+   private static int[] posOfGuesses;
    //private int hunting = 0;
-   private int huntHitCount = 0;
-   private int[] hits;
-   private int[] misses;
-   private int[] guesses;
-   private boolean hunting = false;
-   private boolean dirFound = false;
-   private int huntPoint = 0;
+   private static int huntHitCount = 0;
+   private static int[] hits = new int[100];
+   private static int[] misses = new int[100];
+   private static int[] guesses = new int[100];
+   private static int[][] scoreboard = new int[10][10];
    
-   public Robot()
-   {
-
-   }
-
-   /**
-   * Sends an attack to the scoreboard
-   * Sent to all necessary classes
-   */
+   private static boolean hunting = false;
+   private static boolean dirFound = false;
+   private static int huntPoint = 0;
+   private static int orientation;
    
-   public static boolean validGuess(int row, int col)
+   
+   private static int[][] opponentShips;
+   private static boolean[][] hasBeenAttacked;
+   private static Random random;
+   
+   public Robot(int[][] opponentShips)
    {
-      
-      boolean returnVal = true;
-      
-      if(row > 9 || row < 0)
-      {
-         returnVal = false;
-      }
-      
-      if(col > 9 || col < 0)
-      {
-         returnVal = false;
-      }
-      
-      return returnVal;
-      
+      this.opponentShips = opponentShips;
+      hasBeenAttacked = new boolean[10][10];
+      random = new Random();
    }
    
-   public int randomMove(int row, int col)
+   public static int[] nextAttack()
    {
-      int randomMove = (int)(Math.random() * 3);
-      if(randomMove == 0)
+      int row, col;
+      for(int x = 0; x < opponentShips.length; x++)
       {
-         col++;
-      } else if(randomMove == 1)
-      {
-         row++;
-      } else if(randomMove == 2)
-      {
-         col--;
-      } else
-      {
-         row--;
-      }
-      
-      return row * 10 + col;
-      
-   }
-   
-   public int attack()
-   {
-      
-      turnCounter++;
-      int returnVal = -99;
-      
-      if(turnCounter == 1)
-      {
-         returnVal = (int)(Math.random() * 99);
-      } else
-      {
-         if(hunting)
+         for(int y = 0; y < opponentShips[x].length; y++)
          {
-            if(hits[hits.length - 1] == guesses[guesses.length - 1])
+            if(x > 0 && !hasBeenAttacked[x][y])
             {
-               
-               hunting = false;
-               dirFound = true;
-            } else
-            {
-               int row = (int)Math.floor(huntPoint / 10);
-               int col = huntPoint % 10;
-               
-            int guess = randomMove(row, col); 
-            while(!validGuess((int)Math.floor(guess/10), guess % 10))
-            {
-               guess = randomMove(row, col);
+               row = x - 1;
+               col = y;
+               return new int[]{row, col};
             }
-            //check if guess is a hit
-            //
-            
+
+            if( x < 9 && !hasBeenAttacked[x + 1][y])
+            {
+               row = x + 1;
+               col = y;
+               return new int[]{row, col};
+            }
+
+            if(y > 0 && !hasBeenAttacked[x][y - 1])
+            {
+               row = x;
+               col = y - 1;
+            }
+
+            if(y < 9 && !hasBeenAttacked[x][y + 1])
+            {
+               row = x;
+               col = y+1;
+            }
          }
-         } else if(dirFound)
-            
-         {
-         
-         } else if (hits[hits.length-1] == guesses[guesses.length - 1])
-         {
-            //basically if the last one was a hit, getting you into our "hunting" mechanism
-            hunting = true;
-         } //else if () when the capability to check if something is a hit or not is available, check the dirFound variable
-         //dirFound should update after the hunting mechanism is over
-         else
-         {
-            returnVal = (int)(Math.random() * 99 + 0);
-            while(alreadyGuessed(returnVal, guessed))
-            {
-               returnVal = (int)(Math.random() * 99 + 0);
-            }
-         } 
-         
-         
       }
-      
-      
-      
-      return returnVal;
+
+      do
+      {
+         double bruh = Math.random();
+         double bruh2 = Math.random();
+         System.out.println("" + bruh + " " + bruh2);
+         row = (int)Math.floor((bruh * 10));
+         col = (int)Math.floor((bruh2 * 10));
+         System.out.println(row + " " + col);
+
+      } while (!hasBeenAttacked[row][col]);
+      // guessed[turnCounter - 1] = row * 10 + col;
+      return new int[]{row, col};
    }
 
-   /**
-   * Turns the attack into column row notation for ease of scorekeeper class
-   */
-   public String[] move()
+
+   public static void markAttack(int row, int col)
    {
-      return null;
+      hasBeenAttacked[row][col] = true;
    }
-   
-   /**
-   * Checks how many of the robot's ships exist (number is a placeholder)
-   */
-   
-   public int existingShips()
+
+   public static void main(String[] args)
    {
-      int lastScore[] = new int[2];
-      return 0;
-   }
-   
-   
-   /**
-   * Checks if the square that the AI wants to guess has not already been guessed (boolean is a placeholder)
-   */
-   public boolean alreadyGuessed(int a, int[] g)
-   {
-      boolean returnVal = false;
+      System.out.println("e");
+      int[][] grid = bruh()[0];
+      opponentShips = grid;
+      hasBeenAttacked = new boolean[10][10];
+      
+      for(int y = 0; y < 10; y++)
+      {
+         for(int z = 0; z < 10; z++)
+         {
+            hasBeenAttacked[y][z] = false;
+         }
+      }
       for(int x = 0; x < 100; x++)
       {
-         if(g[x] == a)
-         {
-            returnVal = true;
-         }
+         markAttack(nextAttack()[0], nextAttack()[1]);
       }
 
-      return returnVal;
+
+      System.out.println(Arrays.deepToString(hasBeenAttacked).replace("], ", "]\n"));
+      
+      
+
    }
-   
-   /**
-   * Returns the locations of the ships that the AI decides at the beginning of the game
-   */
-   public static int[][] getShips()
-   {
-      return null;
-   }
-   
    public static int cRandGuess()
    {
       int returnVal = (int)(Math.random() * 99); 
       return returnVal;
    }
+
+
    
-   public static void main(String[] args) {
+
+  public static int[][][] bruh()
+  {
       int[][] grid = new int[10][10];
-      Object[][] ships = new Object[5][4]; // creating 5 ships with [x, y, size, orientation] all stored in them
+      int[][] ships = new int[5][4]; // creating 5 ships with [x, y, size, orientation] all stored in them
 
       // Initialize the grid with 0 (no ships)
       for (int i = 0; i < 10; i++) {
@@ -205,72 +155,64 @@ public class Robot
 
       // Randomly instantiate battleships
       for (int i = 0; i < shipSizes.length; i++) {
-          int size = shipSizes[i];
-          boolean shipPlaced = false;
-          while (!shipPlaced) {
-              // Generate random starting coordinates
-              int x = (int)(Math.random() * 9);
-              int y = (int)(Math.random() * 9);
+         int size = shipSizes[i];
+         boolean shipPlaced = false;
+         while (!shipPlaced) {
+            // Generate random starting coordinates
+            int x = (int)(Math.random() * 9);
+            int y = (int)(Math.random() * 9);
 
-              // Generate random orientation (0 for horizontal, 1 for vertical)
-              int orientation = (int)(Math.random());
+            // Generate random orientation (0 for horizontal, 1 for vertical)
+            int orientation = (int)(Math.random());
 
-              // Check if the battleship can be placed at the generated coordinates
-              boolean canPlaceShip = true;
-              if (orientation == 0) { // horizontal
-                  if (x + size > 10) {
-                      canPlaceShip = false;
-                  } else {
-                      for (int j = x; j < x + size; j++) {
-                          if (grid[j][y] == 1) {
-                              canPlaceShip = false;
-                              break;
-                          }
-                      }
-                  }
-              } else { // vertical
-                  if (y + size > 10) {
-                      canPlaceShip = false;
-                  } else {
-                      for (int j = y; j < y + size; j++) {
-                          if (grid[x][j] == 1) {
-                              canPlaceShip = false;
-                              break;
-                          }
-                      }
-                  }
-              }
-
-              // Place the battleship on the grid if it can be placed
-              if (canPlaceShip) {
-                  if (orientation == 0) { // horizontal
-                      for (int j = x; j < x + size; j++) {
-                          grid[j][y] = 1;
-                      }
-                  } else { // vertical
-                      for (int j = y; j < y + size; j++) {
-                          grid[x][j] = 1;
-                      }
-                  }
-                  ships[i][0] = x;
-                  ships[i][1] = y;
-                  ships[i][2] = size;
-                  ships[i][3] = orientation;
-                  shipPlaced = true;
-
-                  for(int e = 0; e < ships.length; e++)
-                  {
-                     for(int z = 0; z < 4; z++)
-                     
-                     {
-                        System.out.println(ships[e][z] + "");
+            // Check if the battleship can be placed at the generated coordinates
+            boolean canPlaceShip = true;
+            if (orientation == 0) { // horizontal
+               if (x + size > 10) {
+                     canPlaceShip = false;
+               } else {
+                     for (int j = x; j < x + size; j++) {
+                        if (grid[j][y] == 1) {
+                           canPlaceShip = false;
+                           break;
+                        }
                      }
+               }
+            } else { // vertical
+               if (y + size > 10) {
+                     canPlaceShip = false;
+               } else {
+                     for (int j = y; j < y + size; j++) {
+                        if (grid[x][j] == 1) {
+                           canPlaceShip = false;
+                           break;
+                        }
+                     }
+               }
+            }
 
-                  }
-              }
-          }
+            // Place the battleship on the grid if it can be placed
+            if (canPlaceShip) {
+               if (orientation == 0) { // horizontal
+                     for (int j = x; j < x + size; j++) {
+                        grid[j][y] = 1;
+                     }
+               } else { // vertical
+                     for (int j = y; j < y + size; j++) {
+                        grid[x][j] = 1;
+                     }
+               }
+               ships[i][0] = x;
+               ships[i][1] = y;
+               ships[i][2] = size;
+               ships[i][3] = orientation;
+               shipPlaced = true;
+            }
+         }
       }
 
-      
+      int[][][] bruh = new int[][][]{grid, ships};
+      return bruh;
   }
+
 }
